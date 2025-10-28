openxla/xla

[ReplicaGroupV3][MeshAxesReplicaGroupList][1/2] Add initial class definition for V3 replica group.
PiperOrigin-RevId: 820760751

xla/hlo/ir/replica_group_test.cc
@@ -16,10 +16,16 @@ limitations under the License.
 #include "xla/hlo/ir/replica_group.h"
 
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "testing/base/public/mock-log.h"
+#include "absl/base/log_severity.h"
+#include "xla/array.h"
+#include "xla/hlo/ir/mesh_and_axis.h"
+#include "xla/hlo/ir/tile_assignment.h"
 #include "xla/service/hlo.pb.h"
 #include "xla/xla_data.pb.h"
 
@@ -35,6 +41,115 @@ CollectiveDeviceListProto CreateDeviceListProto(
     }
   }
   return proto;
+}
+
+TEST(MeshAxesReplicaGroupListTest, ReplicaGroupsCountAndSize) {
+  Mesh mesh_all_axes(TileAssignment(IotaTileAssignment::Create(
+                         /*dims=*/{4, 4})),
+                     /*axes_names=*/{"x", "y"});
+  MeshAxesReplicaGroupList replica_group_across_all_axes(
+      mesh_all_axes,
+      /*axes=*/{AxisRef(0), AxisRef(1)});
+  EXPECT_EQ(replica_group_across_all_axes.num_replica_groups(), 1);
+  EXPECT_EQ(replica_group_across_all_axes.num_devices_per_group(), 16);
+
+  Mesh mesh_one_axes(TileAssignment(IotaTileAssignment::Create(
+                         /*dims=*/{3, 5})),
+                     /*axes_names=*/{"a", "b"});
+  MeshAxesReplicaGroupList replica_group_across_a(mesh_one_axes,
+                                                  /*axes=*/{AxisRef(0)});
+  MeshAxesReplicaGroupList replica_group_across_b(mesh_one_axes,
+                                                  /*axes=*/{AxisRef(1)});
+  EXPECT_EQ(replica_group_across_a.num_replica_groups(), 5);
+  EXPECT_EQ(replica_group_across_a.num_devices_per_group(), 3);
+  EXPECT_EQ(replica_group_across_b.num_replica_groups(), 3);
+  EXPECT_EQ(replica_group_across_b.num_devices_per_group(), 5);
+
+  Mesh mesh_no_axes(TileAssignment(IotaTileAssignment::Create(
+                        /*dims=*/{2, 3, 5})),
+                    /*axes_names=*/{"p1", "p2", "p3"});
+  testing::ScopedMockLog log(testing::kDoNotCaptureLogsYet);
+  EXPECT_CALL(log,
+              Log(base_logging::WARNING, testing::_,
+                  testing::HasSubstr("has only one device per replica group")))
+      .Times(1);
+  log.StartCapturingLogs();
+  MeshAxesReplicaGroupList replica_group_across_no_axes(mesh_no_axes,
+                                                        /*axes=*/{});
+  EXPECT_EQ(replica_group_across_no_axes.num_replica_groups(), 2 * 3 * 5);
+  EXPECT_EQ(replica_group_across_no_axes.num_devices_per_group(), 1);
+}
+
+TEST(MeshAxesReplicaGroupListTest, ReplicaGroupsCountAndSizeForSubaxes) {
+  Mesh mesh_one_subaxis(TileAssignment(IotaTileAssignment::Create(
+                            /*dims=*/{2, 6, 10})),
+                        /*axes_names=*/{"axis1", "axis2", "axis3"});
+  MeshAxesReplicaGroupList replica_group_across_axis1_subaxis(
+      mesh_one_subaxis,
+      /*axes=*/{AxisRef(0, {1, 2})});
+  MeshAxesReplicaGroupList replica_group_across_axis2_subaxis(
+      mesh_one_subaxis,
+      /*axes=*/{AxisRef(1, {2, 3})});
+  EXPECT_EQ(replica_group_across_axis1_subaxis.num_replica_groups(), 60);
+  EXPECT_EQ(replica_group_across_axis1_subaxis.num_devices_per_group(), 2);
+  EXPECT_EQ(replica_group_across_axis2_subaxis.num_replica_groups(), 40);
+  EXPECT_EQ(replica_group_across_axis2_subaxis.num_devices_per_group(), 3);
+
+  Mesh mesh_multiple_subaxis(TileAssignment(IotaTileAssignment::Create(
+                                 /*dims=*/{2 * 3, 5 * 7, 11 * 13})),
+                             /*axes_names=*/{"alpha", "beta", "gamma"});
+  MeshAxesReplicaGroupList replica_group_across_multiple_subaxis1(
+      mesh_multiple_subaxis,
+      /*axes=*/{AxisRef(0, {1, 2}), AxisRef(1, {1, 5}), AxisRef(2, {1, 11})});
+  MeshAxesReplicaGroupList replica_group_across_multiple_subaxis2(
+      mesh_multiple_subaxis,
+      /*axes=*/{AxisRef(0, {2, 3}), AxisRef(1, {5, 7}), AxisRef(2, {11, 13})});
+  EXPECT_EQ(replica_group_across_multiple_subaxis1.num_replica_groups(), 273);
+  EXPECT_EQ(replica_group_across_multiple_subaxis1.num_devices_per_group(),
+            2 * 5 * 11);
+  EXPECT_EQ(replica_group_across_multiple_subaxis2.num_replica_groups(), 110);
+  EXPECT_EQ(replica_group_across_multiple_subaxis2.num_devices_per_group(),
+            3 * 7 * 13);
+}
+
+TEST(MeshAxesReplicaGroupListTest, MeshAxesToString) {
+  // No subaxes and iota device assignment.
+  Mesh mesh_uvw(TileAssignment(IotaTileAssignment::Create(
+                    /*dims=*/{10, 12, 15})),
+                /*axes_names=*/{"u", "v", "w"});
+  MeshAxesReplicaGroupList replica_group_across_none(mesh_uvw, /*axes=*/{});
+  EXPECT_EQ(replica_group_across_none.ToString(), "@mesh<u=10,v=12,w=15> {}");
+  MeshAxesReplicaGroupList replica_group_across_uv(
+      mesh_uvw,
+      /*axes=*/{AxisRef(0), AxisRef(1)});
+  EXPECT_EQ(replica_group_across_uv.ToString(), "@mesh<u=10,v=12,w=15> {u,v}");
+
+  // Subaxes and replica group v2 iota style device assignment.
+  Mesh mesh_abcd(TileAssignment(IotaTileAssignment::Create(
+                     /*dims=*/{2, 4, 4, 2}, /*reshape_dims=*/{1, 4, 1, 16},
+                     /*transpose_perm=*/{2, 3, 0, 1})),
+                 /*axes_names=*/{"a", "b", "c", "d"});
+  MeshAxesReplicaGroupList rg_abcd_across_none(mesh_abcd, /*axes=*/{});
+  EXPECT_EQ(rg_abcd_across_none.ToString(),
+            "@mesh<a=2,b=4,c=4,d=2>(devices=[2,4,4,2]<=[4,16]T(1,0)) {}");
+  MeshAxesReplicaGroupList rg_abcd_across_multiple_axes_and_subaxes(
+      mesh_abcd, /*axes=*/{AxisRef(0), AxisRef(1, {1, 2}), AxisRef(3)});
+  EXPECT_EQ(
+      rg_abcd_across_multiple_axes_and_subaxes.ToString(),
+      "@mesh<a=2,b=4,c=4,d=2>(devices=[2,4,4,2]<=[4,16]T(1,0)) {a,b:(1)2,d}");
+
+  // Subaxes and random device assignment.
+  Array<int64_t> array({{8, 3, 7, 5, 4, 2, 6, 0, 1, 9}});
+  array.Reshape({10});
+  TileAssignment tile_assignment(std::make_shared<Array<int64_t>>(array));
+  Mesh mesh_ooo(tile_assignment, /*axes_names=*/{"ooo"});
+  MeshAxesReplicaGroupList rg_ooo_across_none(mesh_ooo, /*axes=*/{});
+  EXPECT_EQ(rg_ooo_across_none.ToString(),
+            "@mesh<ooo=10>(devices=[10]8,3,7,5,4,2,6,0,1,9) {}");
+  MeshAxesReplicaGroupList rg_ooo_across_ooo_5_2(mesh_ooo,
+                                                 /*axes=*/{AxisRef(0, {5, 2})});
+  EXPECT_EQ(rg_ooo_across_ooo_5_2.ToString(),
+            "@mesh<ooo=10>(devices=[10]8,3,7,5,4,2,6,0,1,9) {ooo:(5)2}");
 }
 
 TEST(CollectiveDeviceListTest, DefaultListToString) {

ArifMesi/tensorflow

third_party/xla/xla/hlo/ir/replica_group_test.cc
@@ -16,10 +16,16 @@ limitations under the License.
 #include "xla/hlo/ir/replica_group.h"
 
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "testing/base/public/mock-log.h"
+#include "absl/base/log_severity.h"
+#include "xla/array.h"
+#include "xla/hlo/ir/mesh_and_axis.h"
+#include "xla/hlo/ir/tile_assignment.h"
 #include "xla/service/hlo.pb.h"
 #include "xla/xla_data.pb.h"
 
@@ -35,6 +41,117 @@ CollectiveDeviceListProto CreateDeviceListProto(
     }
   }
   return proto;
+}
+
+TEST(MeshAxesReplicaGroupListTest, ReplicaGroupsCountAndSize) {
+  Mesh all_axes(TileAssignment(IotaTileAssignment::Create(
+                    /*dims=*/{4, 4})),
+                /*axes_names=*/{"x", "y"});
+  MeshAxesReplicaGroupList replica_group_across_all_axes(
+      all_axes,
+      /*axes=*/{AxisRef(0), AxisRef(1)});
+  EXPECT_EQ(replica_group_across_all_axes.num_replica_groups(), 1);
+  EXPECT_EQ(replica_group_across_all_axes.num_devices_per_group(), 16);
+
+  Mesh one_axes(TileAssignment(IotaTileAssignment::Create(
+                    /*dims=*/{3, 5})),
+                /*axes_names=*/{"a", "b"});
+  MeshAxesReplicaGroupList replica_group_across_a(one_axes,
+                                                  /*axes=*/{AxisRef(0)});
+  MeshAxesReplicaGroupList replica_group_across_b(one_axes,
+                                                  /*axes=*/{AxisRef(1)});
+  EXPECT_EQ(replica_group_across_a.num_replica_groups(), 5);
+  EXPECT_EQ(replica_group_across_a.num_devices_per_group(), 3);
+  EXPECT_EQ(replica_group_across_b.num_replica_groups(), 3);
+  EXPECT_EQ(replica_group_across_b.num_devices_per_group(), 5);
+
+  Mesh no_axes(TileAssignment(IotaTileAssignment::Create(
+                   /*dims=*/{2, 3, 5})),
+               /*axes_names=*/{"p1", "p2", "p3"});
+  testing::ScopedMockLog log(testing::kDoNotCaptureLogsYet);
+  EXPECT_CALL(log,
+              Log(base_logging::ERROR, testing::_,
+                  testing::HasSubstr("has only one device per replica group")))
+      .Times(1);
+  log.StartCapturingLogs();
+  MeshAxesReplicaGroupList replica_group_across_no_axes(no_axes,
+                                                        /*axes=*/{});
+  EXPECT_EQ(replica_group_across_no_axes.num_replica_groups(), 2 * 3 * 5);
+  EXPECT_EQ(replica_group_across_no_axes.num_devices_per_group(), 1);
+}
+
+TEST(MeshAxesReplicaGroupListTest, ReplicaGroupsCountAndSizeForSubaxes) {
+  Mesh mesh_one_subaxis(TileAssignment(IotaTileAssignment::Create(
+                            /*dims=*/{2, 6, 10})),
+                        /*axes_names=*/{"axis1", "axis2", "axis3"});
+  MeshAxesReplicaGroupList replica_group_across_axis1_subaxis(
+      mesh_one_subaxis,
+      /*axes=*/{AxisRef(0, {1, 2})});
+  MeshAxesReplicaGroupList replica_group_across_axis2_subaxis(
+      mesh_one_subaxis,
+      /*axes=*/{AxisRef(1, {2, 3})});
+  EXPECT_EQ(replica_group_across_axis1_subaxis.num_replica_groups(), 60);
+  EXPECT_EQ(replica_group_across_axis1_subaxis.num_devices_per_group(), 2);
+  EXPECT_EQ(replica_group_across_axis2_subaxis.num_replica_groups(), 40);
+  EXPECT_EQ(replica_group_across_axis2_subaxis.num_devices_per_group(), 3);
+
+  Mesh mesh_multiple_subaxis(TileAssignment(IotaTileAssignment::Create(
+                                 /*dims=*/{2 * 3, 5 * 7, 11 * 13})),
+                             /*axes_names=*/{"alpha", "beta", "gamma"});
+  MeshAxesReplicaGroupList replica_group_across_multiple_subaxis1(
+      mesh_multiple_subaxis,
+      /*axes=*/{AxisRef(0, {1, 2}), AxisRef(1, {1, 5}), AxisRef(2, {1, 11})});
+  MeshAxesReplicaGroupList replica_group_across_multiple_subaxis2(
+      mesh_multiple_subaxis,
+      /*axes=*/{AxisRef(0, {2, 3}), AxisRef(1, {5, 7}), AxisRef(2, {11, 13})});
+  EXPECT_EQ(replica_group_across_multiple_subaxis1.num_replica_groups(),
+            3 * 7 * 13);
+  EXPECT_EQ(replica_group_across_multiple_subaxis1.num_devices_per_group(),
+            2 * 5 * 11);
+  EXPECT_EQ(replica_group_across_multiple_subaxis2.num_replica_groups(),
+            2 * 5 * 11);
+  EXPECT_EQ(replica_group_across_multiple_subaxis2.num_devices_per_group(),
+            3 * 7 * 13);
+}
+
+TEST(MeshAxesReplicaGroupListTest, MeshAxesToString) {
+  // No subaxes and iota device assignment.
+  Mesh mesh_uvw(TileAssignment(IotaTileAssignment::Create(
+                    /*dims=*/{10, 12, 15})),
+                /*axes_names=*/{"u", "v", "w"});
+  MeshAxesReplicaGroupList replica_group_across_none(mesh_uvw, /*axes=*/{});
+  EXPECT_EQ(replica_group_across_none.ToString(), "@mesh<u=10,v=12,w=15> {}");
+  MeshAxesReplicaGroupList replica_group_across_uv(
+      mesh_uvw,
+      /*axes=*/{AxisRef(0), AxisRef(1)});
+  EXPECT_EQ(replica_group_across_uv.ToString(), "@mesh<u=10,v=12,w=15> {u,v}");
+
+  // Subaxes and replica group v2 iota style device assignment.
+  Mesh mesh_abcd(TileAssignment(IotaTileAssignment::Create(
+                     /*dims=*/{2, 4, 4, 2}, /*reshape_dims=*/{1, 4, 1, 16},
+                     /*transpose_perm=*/{2, 3, 0, 1})),
+                 /*axes_names=*/{"a", "b", "c", "d"});
+  MeshAxesReplicaGroupList rg_abcd_across_none(mesh_abcd, /*axes=*/{});
+  EXPECT_EQ(rg_abcd_across_none.ToString(),
+            "@mesh<a=2,b=4,c=4,d=2>(devices=[2,4,4,2]<=[4,16]T(1,0)) {}");
+  MeshAxesReplicaGroupList rg_abcd_across_multiple_axes_and_subaxes(
+      mesh_abcd, /*axes=*/{AxisRef(0), AxisRef(1, {1, 2}), AxisRef(3)});
+  EXPECT_EQ(
+      rg_abcd_across_multiple_axes_and_subaxes.ToString(),
+      "@mesh<a=2,b=4,c=4,d=2>(devices=[2,4,4,2]<=[4,16]T(1,0)) {a,b:(1)2,d}");
+
+  // Subaxes and random device assignment.
+  Array<int64_t> array({{8, 3, 7, 5, 4, 2, 6, 0, 1, 9}});
+  array.Reshape({10});
+  TileAssignment tile_assignment(std::make_shared<Array<int64_t>>(array));
+  Mesh mesh_ooo(tile_assignment, /*axes_names=*/{"ooo"});
+  MeshAxesReplicaGroupList rg_ooo_across_none(mesh_ooo, /*axes=*/{});
+  EXPECT_EQ(rg_ooo_across_none.ToString(),
+            "@mesh<ooo=10>(devices=[10]8,3,7,5,4,2,6,0,1,9) {}");
+  MeshAxesReplicaGroupList rg_ooo_across_ooo_5_2(mesh_ooo,
+                                                 /*axes=*/{AxisRef(0, {5, 2})});
+  EXPECT_EQ(rg_ooo_across_ooo_5_2.ToString(),
+            "@mesh<ooo=10>(devices=[10]8,3,7,5,4,2,6,0,1,9) {ooo:(5)2}");
 }
 
 TEST(CollectiveDeviceListTest, DefaultListToString) {